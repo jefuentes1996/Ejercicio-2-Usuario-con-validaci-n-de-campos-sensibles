c60d4836e480da16c0df3e8d47171d51a130ce11
Laptop <Laptop@DESKTOP-8660DAQ>
2025-07-29 21:38:11 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 2 Usuario con validación de campos sensibles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Usuario
+{
+    public string Nombre { get; set; }
+
+    private string dpi;
+    private string contrasena;
+
+    public string DPI
+    {
+        get { return dpi; }
+        set
+        {
+            if (value.Length == 13 && long.TryParse(value, out _))
+            {
+                dpi = value;
+            }
+            else
+            {
+                Console.WriteLine("DPI inválido. Debe contener 13 dígitos.");
+            }
+        }
+    }
+
+    public string Contrasena
+    {
+        set
+        {
+            if (value.Length >= 6)
+            {
+                contrasena = value;
+            }
+            else
+            {
+                Console.WriteLine("Contraseña inválida. Debe tener al menos 6 caracteres.");
+            }
+        }
+    }
+
+    public bool Autenticar(string input)
+    {
+        return contrasena == input;
+    }
+}
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Usuario usuario = new Usuario();
+
+        usuario.Nombre = "Eduardo Fuentes";
+        usuario.DPI = "3303708551202";
+        usuario.Contrasena = "Apolo2025";
+
+        Console.Write("Ingrese su contraseña: ");
+        string input = Console.ReadLine();
+
+        if (usuario.Autenticar(input))
+        {
+            Console.WriteLine("Acceso autorizado");
+        }
+        else
+        {
+            Console.WriteLine("Contraseña incorrecta.");
+        }
+    }
+}
